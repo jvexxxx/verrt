20e018987b405c4d2c5ec84d3e187f7b070181fb
jvexxxx <botlike1yt@gmail.com>
2025-01-06 12:19:16 -0300

    Add files via upload

---
+++ me/levansj01/cz.java
@@ -0,0 +1,17 @@
+package me.levansj01;
+
+public interface cz {
+  public static final int drk;
+  
+  public static final boolean drl = false;
+  
+  double getWalkSpeed();
+  
+  void handle(hl<?> paramhl);
+}
+
+
+/* Location:              C:\Users\Administrator\Desktop\jvg\sv spigot\pl\verus_b4157.jar!\me\levansj01\cz.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
