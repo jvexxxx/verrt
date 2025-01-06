c2c9df28dd88892efa5d6869d7fc1dc200e2b732
jvexxxx <botlike1yt@gmail.com>
2025-01-06 12:18:00 -0300

    Add files via upload

---
+++ me/levansj01/ca.java
@@ -0,0 +1,25 @@
+package me.levansj01;
+
+import java.util.Queue;
+
+public interface ca {
+  public static final int dsy;
+  
+  public static final boolean dsz = false;
+  
+  boolean possibleVelocity();
+  
+  int getTicks();
+  
+  Queue<ez> getQueue();
+  
+  void handle(oe<?> paramoe);
+  
+  void handle(hs<?> paramhs);
+}
+
+
+/* Location:              C:\Users\Administrator\Desktop\jvg\sv spigot\pl\verus_b4157.jar!\me\levansj01\ca.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
