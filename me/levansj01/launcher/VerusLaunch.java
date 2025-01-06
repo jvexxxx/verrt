21a8dd940f9d7097524d55281e19ef78bf3a0a04
jvexxxx <botlike1yt@gmail.com>
2025-01-06 12:13:10 -0300

    Add files via upload

---
+++ me/levansj01/launcher/VerusLaunch.java
@@ -0,0 +1,17 @@
+package me.levansj01.launcher;
+
+public interface VerusLaunch {
+  public static final int cvy;
+  
+  public static final boolean cvz = false;
+  
+  void launch(VerusLauncher paramVerusLauncher);
+  
+  void shutdown(VerusLauncher paramVerusLauncher);
+}
+
+
+/* Location:              C:\Users\Administrator\Desktop\jvg\sv spigot\pl\verus_b4157.jar!\me\levansj01\launcher\VerusLaunch.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
